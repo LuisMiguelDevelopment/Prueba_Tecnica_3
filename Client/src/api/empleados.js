ff99911342e88d4b999f882e8ede6539055359ac
Luis Miguel <luismiguelalvarezmarin109@gmail.com>
2023-11-05 17:36:47 -0500

    fix: :bug: correcion rutas api

--- Client/src/api/empleados.js
+++ Client/src/api/empleados.js
@@ -2,5 +2,5 @@ import axios from './axios';
 
 export const obtenerEmpleadoss = () => axios.get('/empleados');
 export const crearEmpleado = (empleado) => axios.post('/empleados',empleado);
-export const actualizarEmpleado = () => axios.put('/empleados');
-export const deleteEmpleado = () => axios.delete('/empleados');
+export const actualizarEmpleado = (id, formData) => axios.put(`/empleados${id}`,formData);
+export const deleteEmpleado = (id) => axios.delete(`/empleados${id}`);

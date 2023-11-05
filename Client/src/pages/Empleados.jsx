c04a5cfcc8e43da738e6f629b809825a027dfa2e
Luis Miguel <luismiguelalvarezmarin109@gmail.com>
2023-11-05 18:00:01 -0500

    fix: :bug: Cambio para eliminar y mostarr el  area _id

--- Client/src/pages/Empleados.jsx
+++ Client/src/pages/Empleados.jsx
@@ -3,15 +3,21 @@ import { useEmpleados } from '../context/empleadoContext';
 import { useRoles } from '../context/rolesContext';
 import { useAreas } from '../context/areasContext';
 import { useForm } from 'react-hook-form';
-import {AiFillEdit , AiFillDelete} from 'react-icons/ai';
+import { AiFillEdit, AiFillDelete } from 'react-icons/ai';
 import './Empleados.css';
 
 const Empleados = () => {
-  const { empleados, obtenerEmpleados, agregarEmpleado , actualizarEmpleados , deleteEmpleados } = useEmpleados();
+  const { empleados, obtenerEmpleados, agregarEmpleado, deleteEmpleados } = useEmpleados();
   const { roles, obtenerRoles } = useRoles();
   const { areas, obtenerAreas } = useAreas();
   const { register, handleSubmit } = useForm();
   const [quiereBoletin, setQuiereBoletin] = useState(false);
+
+  // Crear un objeto de mapeo de ID de área a nombre de área
+  const areaNameMap = areas.reduce((acc, area) => {
+    acc[area.id] = area.nombre;
+    return acc;
+  }, {});
 
   useEffect(() => {
     obtenerEmpleados();
@@ -19,14 +25,13 @@ const Empleados = () => {
     obtenerAreas();
   }, []);
 
-
   const handleQuiereBoletinChange = (event) => {
     setQuiereBoletin(event.target.checked);
   };
 
-  const handleDeleteEmpleado = (empleadoId)=>{
-    deleteEmpleados(empleadoId)
-  }
+  const handleDeleteEmpleado = (empleadoId) => {
+    deleteEmpleados(empleadoId);
+  };
 
   const onSubmit = handleSubmit((data) => {
     const empleadoData = {
@@ -42,24 +47,21 @@ const Empleados = () => {
 
   return (
     <div className='general'>
-
       <div className="general__form">
         <div className="info">
-          <span>los campos con * son obligatorios</span>
+          <span>Los campos con * son obligatorios</span>
         </div>
-
         <form className="form" action="" onSubmit={onSubmit}>
-
           <div className="form__items">
-            <label className='form__label' htmlFor="">Nombre Completo *</label>
-            <input className='form__input' type="text" name='nombre' {...register("nombre")} />
+            <label className='form__label' htmlFor="nombre">Nombre Completo *</label>
+            <input className='form__input' type="text" id="nombre" name="nombre" {...register("nombre")} />
           </div>
           <div className="form__items">
-            <label className='form__label' htmlFor="">Correo Electrónico *</label>
-            <input className='form__input' type="text" {...register("email")} />
+            <label className='form__label' htmlFor="email">Correo Electrónico *</label>
+            <input className='form__input' type="text" id="email" {...register("email")} />
           </div>
           <div className="sexo__general">
-            <label htmlFor="">Sexo *</label>
+            <label htmlFor="sexo">Sexo *</label>
             <div className="form__sexo">
               <div className="sexo">
                 <label className='sexo' htmlFor="masculino">Masculino</label>
@@ -71,19 +73,18 @@ const Empleados = () => {
               </div>
             </div>
           </div>
-          <select className='select' name="" id="" {...register("area_id")}>
+          <select className='select' name="area_id" {...register("area_id")}>
             {areas.map((area) => (
               <option key={area.id} value={area.id}>
                 {area.nombre}
               </option>
             ))}
           </select>
-          <textarea className='textarea' name="" id="" cols="30" rows="10" placeholder='Descripción de la experiencia del empleado'  {...register("descripcion")}></textarea>
-
+          <textarea className='textarea' name="descripcion" cols="30" rows="10" placeholder='Descripción de la experiencia del empleado' {...register("descripcion")}></textarea>
           <div className="form__areas">
             <div className="boletin">
               <input type="checkbox" onChange={handleQuiereBoletinChange} />
-              <label htmlFor="">Quiere recibir el boletín?</label>
+              <label htmlFor="boletin">Quiere recibir el boletín?</label>
             </div>
             {roles.map((role) => (
               <div className='roles' key={role.id}>
@@ -91,18 +92,13 @@ const Empleados = () => {
                   <input type="checkbox" />
                   {role.nombre}
                 </label>
-                <input
-                  type="hidden"
-                  name="roles"
-                />
+                <input type="hidden" name="roles" />
               </div>
             ))}
           </div>
           <button className='button'>Guardar</button>
         </form>
       </div>
-
-
       <table>
         <tr>
           <th>Nombre</th>
@@ -110,20 +106,20 @@ const Empleados = () => {
           <th>Sexo</th>
           <th>Area</th>
           <th>Boletin</th>
+          <th>Acciones</th>
         </tr>
-        {empleados.map((empleado , index) => (
+        {empleados.map((empleado, index) => (
           <tr key={empleado.id} className={index % 2 === 0 ? 'fila-par' : 'fila-impar'}>
             <td>{empleado.nombre}</td>
             <td>{empleado.email}</td>
             <td>{empleado.sexo}</td>
-            <td>{empleado.area}</td>
+            <td>{areaNameMap[empleado.area_id]}</td>
             <td>{empleado.boletin}</td>
-            <td><AiFillEdit/></td>
-            <td  ><AiFillDelete onClick={()=>handleDeleteEmpleado(empleado.id)}/></td>
+            <td><AiFillEdit /></td>
+            <td><AiFillDelete onClick={() => handleDeleteEmpleado(empleado.id)} /></td>
           </tr>
-            ))}
+        ))}
       </table>
-
     </div>
   );
 }
